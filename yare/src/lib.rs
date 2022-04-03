e4117eb7a15b08d12c011ed84bc6ecbc456bcd0b
Mythridor <enoubia@gmail.com>
2022-04-03 13:42:41 +0200

    chores: Simplified module usage in test

--- yare/src/lib.rs
+++ yare/src/lib.rs
@@ -2,13 +2,13 @@ mod geometry;
 
 #[cfg(test)]
 mod tests {
-    use crate::geometry::{self, VectorialCalculus};
+    use crate::geometry::{self, Point, Vector, VectorialCalculus};
 
     #[test]
     fn vector_definition() {
-        let a: geometry::Point = geometry::Point{ x:2, y:3};
-        let b: geometry::Point = geometry::Point{ x:10, y:5};
+        let a: Point = Point{ x:2, y:3};
+        let b: Point = Point{ x:10, y:5};
 
-        assert_eq!(geometry::Point::define_vector(a, b), geometry::Vector {dx: 8, dy:2});
+        assert_eq!(Point::define_vector(a, b), Vector {dx: 8, dy:2});
     }
 }
