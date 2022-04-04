831678395a792876124b0349f22d79f3d1cec177
Mythridor <enoubia@gmail.com>
2022-04-04 15:41:13 +0200

    feat: Added enum to start impl behaviours

---
+++ yare/src/geometry/models.rs
@@ -0,0 +1,13 @@
+enum structure {
+    star,
+    outpost,
+    base,
+}
+
+
+//TODO: Implement function injection as behaviour selector during match
+enum spirit_specialty {
+    soldier,      //Soldiers defends and attacks at will
+    harvester,    //Harvesters have basic implementation of energy harvesting
+    provisionner, //Provisionners convoys energy from one source to a destination
+}
