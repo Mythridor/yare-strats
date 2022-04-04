e9885bb1cb45cd70654042c493f2f7f9a7d557ed
Mythridor <enoubia@gmail.com>
2022-04-04 21:23:20 +0200

    fix: Updated values accordingly to type

--- yare/src/tests.rs
+++ yare/src/tests.rs
@@ -4,9 +4,9 @@ mod geo_tests {
 
     #[test]
     fn vector_definition() {
-        let a: Point = Point{ x:2, y:3};
-        let b: Point = Point{ x:10, y:5};
+        let a: Point = Point{ x:2.0, y:3.0};
+        let b: Point = Point{ x:10.0, y:5.0};
 
-        assert_eq!(Point::define_vector(a, b), Vector {dx: 8, dy:2});
+        assert_eq!(Point::define_vector(a, b), Vector {dx: 8.0, dy:2.0});
     }
 }
