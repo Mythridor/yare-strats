39eb553c7ee437c4eeb0e0b2a2d7184db3367057
Mythridor <enoubia@gmail.com>
2022-04-04 20:20:12 +0200

    fix: Made SpiritSpecialty public

--- yare/src/geometry/models.rs
+++ yare/src/geometry/models.rs
@@ -6,7 +6,7 @@ pub enum Structure {
 
 
 //TODO: Implement function injection as behaviour selector during match
-enum SpiritSpecialty {
+pub enum SpiritSpecialty {
     Soldier,      //Soldiers defends and attacks at will
     Harvester,    //Harvesters have basic implementation of energy harvesting
     Provisionner, //Provisionners convoys energy from one source to a destination
