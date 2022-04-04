b969ed4eb12e4354e43f868715082c637e3d3613
Mythridor <enoubia@gmail.com>
2022-04-04 19:56:31 +0200

    feat: First implementation of Distance trait

---
+++ yare/src/geometry/distance.rs
@@ -0,0 +1,19 @@
+use crate::geometry::Point;
+use crate::geometry::models::Structure;
+
+pub trait Distance {
+
+    /// Get the distance betwenn two points.
+    ///
+    /// Given that the destination point is a Point. 
+    /// It.calculate distance between two points.
+    fn calculate_distance(self, to: Point) -> u32;
+
+    /// Get the closest Structure.
+    ///
+    /// Given that the destination point is a structure defined in the enum and that it returns the closest one. 
+    /// A match will to behave accordingly to the nature of the structure.
+    fn define_closest_structure() -> Structure;
+
+
+}
